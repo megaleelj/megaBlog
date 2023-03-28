30415f6a9c3446b3f48bc509b91664e1e53977c2
Osemeigah Igbafe <2046969@csl-student.wlv.ac.uk>
2023-03-28 16:51:05 +0100

    sepreating the login and register in the welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,19 @@
       <div class="card shadow-lg p-3 mb-5 bg-white rounded">
         <div class="card-header bg-dark text-light">{{ __('Welcome to MegaBlog') }}</div>
 
-        <div class="card-body">
-          <p class="lead">Thank you for visiting MegaBlog! Here, you can read blogs from our community of writers, or create your own blog if you're a registered user.</p>
+              <div class="card-body">
+              <p class="lead">Thank you for visiting MegaBlog! Here, you can read blogs from our community of writers, or create your own blog if you're a registered user.</p>
 
-          @if(Auth::guest())
-            <p class="text-center"><a class="btn btn-lg btn-primary" href="{{ URL('/login') }}">Login or Register</a></p>
-          @else
-            <p class="lead text-center">To create a new blog, simply click on the "New Blog" button on the top navigation bar. You can also browse all blogs by clicking on the "Blogs" link.</p>
-          @endif
-        </div>
+                @if(Auth::guest())
+                <p class="text-center">
+                  <a class="btn btn-lg btn-primary" href="{{ URL('/login') }}">Login</a>
+                  <a class="btn btn-lg btn-secondary" href="{{ URL('/register') }}">Register</a>
+                </p>
+              @else
+                <p class="lead text-center">To create a new blog, simply click on the "New Blog" button on the top navigation bar. You can also browse all blogs by clicking on the "Blogs" link.</p>
+              @endif
+           </div>
+         </div>
       </div>
     </div>
   </div>
